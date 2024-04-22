6049d59c28af717af0d43c695b24735a2e98bc7c
zidanekt <zidanemafruhi@gmail.com>
2024-04-22 14:32:07 +0700

    database

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const mysql = require("mysql");
 const db = mysql.createConnection({
   host: "sql6.freesqldatabase.com",
   port: 3306,
-  user: "sql6695748",
-  password: "8lRg3fRWxR",
-  database: "sql6695748",
+  user: "sql6700950",
+  password: "6bnd24nX9h",
+  database: "sql6700950",
 });
 
 db.connect((err) => {
